67fe636b6455dbc2abd22b755476b018fd0a1751
wardymate <chriswarddev@gmail.com>
2015-03-11 14:48:01 +0000

    updated tumblr blog handler with uniform methods

--- src/TumblrPostHandler.js
+++ src/TumblrPostHandler.js
@@ -2,60 +2,41 @@ var TumblrPostHandler = function(blogPost){
   this.post = [blogPost]
 };
 
-TumblrPostHandler.prototype.imageUrl = function() {
-  if (this.post[0].type === "photo") {
-    var url;
-    url = this.post[0].photos[0].original_size.url;
-    return url;}
-  else {
-    return '';
-  }
-};
-
 TumblrPostHandler.prototype.blogDate = function() {
   var date;
   date = this.post[0].date;
   return date;
 };
 
-TumblrPostHandler.prototype.graphicCaption = function() {
+TumblrPostHandler.prototype.blogTitle = function() {
   if (this.post[0].type === "photo" || this.post[0].type === "video") {
     var graphicCaption;
     graphicCaption = this.post[0].caption;
     return graphicCaption;}
   else {
-    return '';
-  }
-};
-
-TumblrPostHandler.prototype.textBody = function() {
-  if (this.post[0].type === "text") {
-    var textbody;
-    textbody = this.post[0].body;
-    return textbody;}
-  else {
-    return '';
-  }
-};
-
-TumblrPostHandler.prototype.textTitle = function() {
-  if (this.post[0].type === "text") {
     var textTitle;
     textTitle = this.post[0].title;
     return textTitle;}
-  else {
-    return '';
-  }
 };
 
-TumblrPostHandler.prototype.blogLink = function() {
+TumblrPostHandler.prototype.blogBackLink = function() {
   var blogLink;
   blogLink = this.post[0].post_url;
   return blogLink;
 };
 
-TumblrPostHandler.prototype.hyperLink = function() {
-  if (this.post[0].type === 'link'){
+TumblrPostHandler.prototype.blogSource = function() {
+  var blogSource;
+  blogSource = this.post[0].blog_name;
+  return blogSource;
+};
+
+TumblrPostHandler.prototype.blogMedia = function() {
+  if (this.post[0].type === "photo") {
+    var blogMedia;
+    blogMedia = this.post[0].photos[0].original_size.url;
+    return blogMedia;}
+  else if(this.post[0].type === "link"){
     var hyperLink;
     hyperLink = this.post[0].url;
     return hyperLink;}
@@ -64,25 +45,69 @@ TumblrPostHandler.prototype.hyperLink = function() {
   }
 };
 
-TumblrPostHandler.prototype.linkDescription = function() {
-  if (this.post[0].type === 'link'){
-    var linkDescription;
-    linkDescription = this.post[0].description;
-    return linkDescription;}
+TumblrPostHandler.prototype.blogText = function() {
+  if (this.post[0].type === "text") {
+    var blogText;
+    blogText = this.post[0].body;
+    return blogText;}
   else {
     return '';
   }
 };
 
-TumblrPostHandler.prototype.videoUrl = function() {
-  if (this.post[0].type === "video"){
-    var videoUrl;
-    videoUrl = this.post[0].video_url;
-    return videoUrl;}
-  else {
-    return '';
-  }
-};
+// TumblrPostHandler.prototype.graphicCaption = function() {
+//   if (this.post[0].type === "photo" || this.post[0].type === "video") {
+//     var graphicCaption;
+//     graphicCaption = this.post[0].caption;
+//     return graphicCaption;}
+//   else {
+//     return '';
+//   }
+// };
+
+
+
+// TumblrPostHandler.prototype.textTitle = function() {
+//   if (this.post[0].type === "text") {
+//     var textTitle;
+//     textTitle = this.post[0].title;
+//     return textTitle;}
+//   else {
+//     return '';
+//   }
+// };
+
+// TumblrPostHandler.prototype.hyperLink = function() {
+//   if (this.post[0].type === 'link'){
+//     var hyperLink;
+//     hyperLink = this.post[0].url;
+//     return hyperLink;}
+//   else {
+//     return '';
+//   }
+// };
+
+// TumblrPostHandler.prototype.linkDescription = function() {
+//   if (this.post[0].type === 'link'){
+//     var linkDescription;
+//     linkDescription = this.post[0].description;
+//     return linkDescription;}
+//   else {
+//     return '';
+//   }
+// };
+
+// TumblrPostHandler.prototype.videoUrl = function() {
+//   if (this.post[0].type === "video"){
+//     var videoUrl;
+//     videoUrl = this.post[0].video_url;
+//     return videoUrl;}
+//   else {
+//     return '';
+//   }
+// };
+
+
 
 
 
